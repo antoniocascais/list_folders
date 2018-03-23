89303c8ea858dce1dc56629e5c890cbf45fa094c
António Cascais <antonio.cascais@springer.com>
2018-03-23 22:55:24 +0100

    Fixed bug with updating parent
    Function needs to be recursive in order to get the sum of all the sub
    folders files

--- struct/listContent.py
+++ struct/listContent.py
@@ -1,24 +1,31 @@
 import os
 
 
-def update_parent(files_by_folder, file, num_files):
+def update_parent(starting_dir, files_by_folder, file, num_files):
     n = 0
     parent = os.path.dirname(file)
+
+    if starting_dir is file:
+        return files_by_folder
+
     if parent in files_by_folder:
         n = files_by_folder[parent]
     files_by_folder[parent] = n + num_files
-    return
+
+    return update_parent(starting_dir, files_by_folder, parent, num_files)
 
 
 def main():
     files_by_folder = dict()
-    for root, dirs, files in os.walk(u".", topdown=False):
+    starting_dir = u"."
+    for root, dirs, files in os.walk(starting_dir, topdown=False):
         n_files = len(files)
-        update_parent(files_by_folder, root, n_files)
+        files_by_folder = update_parent(starting_dir, files_by_folder, root, n_files)
         if root in files_by_folder:
-            print(root, "contains", files_by_folder[root], "files")
+            n = files_by_folder[root]
         else:
-            print(root, "contains", n_files, "files")
+            n = n_files
+        print(root, "contains", n, "files")
     return
 
 
