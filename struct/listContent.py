dfff47eb721ad6c3af5aa1454aa3d87292bf4490
António Cascais <antonio.cascais@springer.com>
2018-03-23 22:40:52 +0100

    Refactored pretty much everything
    The walk method that the os library provides does what we want, no need
    to reinvent the wheel.

--- struct/listContent.py
+++ struct/listContent.py
@@ -1,45 +1,24 @@
 import os
 
 
-def contains_subfolders(file):
-    if not os.path.isdir(file):
-        return False
-    path = os.path.realpath(file)
-    for f in os.listdir(file):
-        sub_file = path + "/" + f
-        if os.path.isdir(sub_file):
-            return True
-    return False
-
-
-def process_file(visited_files, file):
-    print("file",file)
-    files = os.listdir(file)
-    for f in files:
-        path = os.path.realpath(f)
-        print("path",path)
-        file_with_path = path + "/" + f
-        print("file with opath", file_with_path)
-        if file_with_path not in visited_files:
-            visited_files.append(file_with_path)
-            if contains_subfolders(file_with_path):
-                process_file(visited_files, file_with_path)
-            else:
-                print(file_with_path, " contains ", str(len(files)), " files.")
-                parent_dir = os.path.dirname(file_with_path)
-                process_file(visited_files, parent_dir)
-        else:
-            path = os.path.realpath(file_with_path)
-            print("path", path)
-            print("not visited", file_with_path)
-
-            file_with_path = os.path.realpath(file_with_path)
-            process_file(visited_files, file_with_path)
+def update_parent(files_by_folder, file, num_files):
+    n = 0
+    parent = os.path.dirname(file)
+    if parent in files_by_folder:
+        n = files_by_folder[parent]
+    files_by_folder[parent] = n + num_files
+    return
 
 
 def main():
-    visited_files = []
-    process_file(visited_files, os.curdir)
+    files_by_folder = dict()
+    for root, dirs, files in os.walk(u".", topdown=False):
+        n_files = len(files)
+        update_parent(files_by_folder, root, n_files)
+        if root in files_by_folder:
+            print(root, "contains", files_by_folder[root], "files")
+        else:
+            print(root, "contains", n_files, "files")
     return
 
 
